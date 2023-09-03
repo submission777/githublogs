87ad0507756e65aca26612dab5c95869887dbb9d
arnoldmac <arni1995@gmail.com>
2023-09-03 17:42:45 +0300

    change from http api to rest api gw

--- aws-logic-infra/api_gw.tf
+++ aws-logic-infra/api_gw.tf
@@ -1,57 +1,73 @@
-resource "aws_apigatewayv2_api" "lambda" {
+resource "aws_api_gateway_rest_api" "gw_proxy" {
   name = "lambdaProxy"
   description = "Proxy to handle requests to lambda function"
-  protocol_type = "HTTP"
-
-  depends_on = [aws_lambda_function.lambda]
 }
 
-resource "aws_cloudwatch_log_group" "main_api_gw" {
-  name = "/aws/api-gw/${aws_apigatewayv2_api.lambda.name}"
-  retention_in_days = 30
+resource "aws_api_gateway_resource" "resource" {
+  parent_id   = aws_api_gateway_rest_api.gw_proxy.root_resource_id
+  path_part   = aws_lambda_function.lambda.function_name
+  rest_api_id = aws_api_gateway_rest_api.gw_proxy.id
 }
 
-resource "aws_apigatewayv2_stage" "lambda" {
-  name          = "lambdaProxy_stage"
-  api_id        = aws_apigatewayv2_api.lambda.id
-  auto_deploy = true
+resource "aws_api_gateway_method" "proxy" {
+  rest_api_id = aws_api_gateway_rest_api.gw_proxy.id
+  resource_id = aws_api_gateway_resource.resource.id
+  http_method = "POST"
+  authorization = "NONE"
+}
 
-  access_log_settings {
-    destination_arn = aws_cloudwatch_log_group.main_api_gw.arn
+resource "aws_api_gateway_integration" "lambda" {
+  http_method = aws_api_gateway_method.proxy.http_method
+  resource_id = aws_api_gateway_method.proxy.resource_id
+  rest_api_id = aws_api_gateway_rest_api.gw_proxy.id
+  type        = "AWS_PROXY"
+  integration_http_method = "POST"
+  uri = aws_lambda_function.lambda.invoke_arn
+}
 
-    format = jsonencode({
-      requestId               = "$context.requestId"
-      sourceIp                = "$context.identity.sourceIp"
-      requestTime             = "$context.requestTime"
-      protocol                = "$context.protocol"
-      httpMethod              = "$context.httpMethod"
-      resourcePath            = "$context.resourcePath"
-      routeKey                = "$context.routeKey"
-      status                  = "$context.status"
-      responseLength          = "$context.responseLength"
-      integrationErrorMessage = "$context.integrationErrorMessage"
+data "aws_iam_policy_document" "gw_policy" {
+  statement {
+    effect = "Allow"
+    principals {
+      identifiers = ["*"]
+      type        = "AWS"
     }
-    )
+    actions   = ["execute-api:Invoke"]
+    resources = ["${aws_api_gateway_rest_api.gw_proxy.execution_arn}/*"]
+  }
+  statement {
+    effect = "Deny"
+    principals {
+      identifiers = ["*"]
+      type        = "AWS"
+    }
+    actions   = ["execute-api:Invoke"]
+    resources = ["${aws_api_gateway_rest_api.gw_proxy.execution_arn}/*"]
+    condition {
+      test     = "NotIpAddress"
+      values   = ["192.30.252.0/22", "185.199.108.0/22", "140.82.112.0/20", "143.55.64.0/20"]
+      variable = "aws:SourceIp"
+    }
   }
 }
 
-resource "aws_apigatewayv2_integration" "integration" {
-  api_id = aws_apigatewayv2_api.lambda.id
-  integration_uri = aws_lambda_function.lambda.invoke_arn
-  integration_type = "AWS_PROXY"
-  integration_method = "POST"
+resource "aws_api_gateway_rest_api_policy" "github_only" {
+  policy      = data.aws_iam_policy_document.gw_policy.json
+  rest_api_id = aws_api_gateway_rest_api.gw_proxy.id
 }
 
-resource "aws_apigatewayv2_route" "route" {
-  api_id      = aws_apigatewayv2_api.lambda.id
-  route_key   = "POST /"
-  target = "integrations/${aws_apigatewayv2_integration.integration.id}"
+resource "aws_api_gateway_deployment" "deployment" {
+  depends_on = [
+    aws_api_gateway_integration.lambda,
+    aws_api_gateway_rest_api_policy.github_only]
+  rest_api_id = aws_api_gateway_rest_api.gw_proxy.id
+  stage_name = "prod"
 }
 
 resource "aws_lambda_permission" "api_gw" {
-  statement_id = "AllowExecutionFromAPIGateway"
+  statement_id = "AllowAPIGatewayInvoke"
   action = "lambda:InvokeFunction"
   function_name = aws_lambda_function.lambda.function_name
   principal = "apigateway.amazonaws.com"
-  source_arn = "${aws_apigatewayv2_api.lambda.execution_arn}/*/*"
+  source_arn = "${aws_api_gateway_rest_api.gw_proxy.execution_arn}/*/*"
 }
--- aws-logic-infra/outputs.tf
+++ aws-logic-infra/outputs.tf
@@ -1,5 +1,6 @@
 output "api_base_url" {
-  value = "${aws_apigatewayv2_stage.lambda.invoke_url}/"
+  #value = "${aws_apigatewayv2_stage.lambda.invoke_url}/"
+  value = "${aws_api_gateway_deployment.deployment.invoke_url}/${aws_api_gateway_resource.resource.path_part}"
 }
 
 output "db_name" {
